b334fb27ef6f6b8e115059ee04fa25d4a8beca07
lijinlin <jinlin.li@haitun88.com>
2021-03-22 08:02:12 +0800

    二叉排序树，中序遍历

---
+++ src/main/java/com/lijinlin/project/learn/datastructureandalgorithm/binarysorttree/BinarySortTreeDemo.java
@@ -0,0 +1,74 @@
+package com.lijinlin.project.learn.datastructureandalgorithm.binarysorttree;
+
+public class BinarySortTreeDemo {
+    public static void main(String[] args) {
+        int[] arr = {7, 3, 10, 12, 5, 1, 9};
+        BinarySortTree binarySortTree = new BinarySortTree();
+        for (int i = 0; i < arr.length; i++) {
+            binarySortTree.add(new Node(arr[i]));
+        }
+        System.out.println("中序遍历二叉排序树");
+        binarySortTree.infixOrder();
+    }
+}
+
+//创建二叉排序树
+class BinarySortTree {
+    private Node root;
+
+    public void add(Node node) {
+        if (root == null) {
+            root = node;
+        } else {
+            root.add(node);
+        }
+    }
+
+    public void infixOrder() {
+        if (root != null) {
+            root.infixOrder();
+        } else {
+            System.out.println("二叉排序树为空不能遍历");
+        }
+    }
+}
+
+class Node {
+    private int value;
+    private Node left;
+    private Node right;
+
+    public Node(int value) {
+        this.value = value;
+    }
+
+    public void add(Node node) {
+        if (node == null) {
+            return;
+        }
+        if (node.value < this.value) {
+            if (this.left == null) {
+                this.left = node;
+            } else {
+                this.left.add(node);
+            }
+        } else {
+            if (this.right == null) {
+                this.right = node;
+            } else {
+                this.right.add(node);
+            }
+        }
+    }
+
+    //中序遍历
+    public void infixOrder() {
+        if (this.left != null) {
+            this.left.infixOrder();
+        }
+        System.out.println(this.value);
+        if (this.right != null) {
+            this.right.infixOrder();
+        }
+    }
+}
