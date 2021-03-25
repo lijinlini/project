332debec0a5fd691f5dc3f45d6bfe628629810ec
jl.li21 <Ljl139691425512>
2021-03-25 15:53:44 +0800

    BIO模型

--- src/main/java/com/lijinlin/project/learn/io/bio/BIOServer.java
+++ src/main/java/com/lijinlin/project/learn/io/bio/BIOServer.java
@@ -6,7 +6,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-
+//BIO模型
 public class BIOServer {
     //线程池机制
     //思路
@@ -20,13 +20,14 @@ public class BIOServer {
 
         while(true){
             //监听，等待客户端连接
+            System.out.println("等待连接");
             final Socket socket = serverSocket.accept();
             System.out.println("链接到一个客户端");
             //就创建一个线程池，与之通讯（单独写一个方法）
             newCachedThreadPool.execute(new Runnable() {
                 @Override
                 public void run() {
-
+                    handler(socket);
                 }
             });
         }
@@ -35,12 +36,22 @@ public class BIOServer {
     //编写一个handler方法，和客户端通讯
     public static void handler(Socket socket){
         try{
+            System.out.println("线程信息 id=" + Thread.currentThread().getId() +
+                    "名字=" + Thread.currentThread().getName());
             byte[] bytes = new byte[1024];
             //通过socket获取一个输入流
             InputStream inputStream = socket.getInputStream();
             //循环读取客户端发送的数据
             while(true){
+                System.out.println("线程信息 id=" + Thread.currentThread().getId() +
+                        "名字=" + Thread.currentThread().getName());
+                System.out.println("read.....");
                 int read = inputStream.read(bytes);
+                if(read != 1){
+                    System.out.println(new String(bytes,0,read));
+                }else{
+                    break;
+                }
             }
 
         }catch (Exception e){
