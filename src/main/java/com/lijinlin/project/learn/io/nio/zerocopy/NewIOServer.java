2c06cb873a840d24eb879bf7f0f3aa6ab69237ae
lijinlin <jinlin.li@haitun88.com>
2021-03-30 23:45:39 +0800

    零拷贝

---
+++ src/main/java/com/lijinlin/project/learn/io/nio/zerocopy/NewIOServer.java
@@ -0,0 +1,32 @@
+package com.lijinlin.project.learn.io.nio.zerocopy;
+
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.nio.ByteBuffer;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+
+public class NewIOServer {
+    public static void main(String[] args) throws Exception {
+        InetSocketAddress address = new InetSocketAddress(7001);
+        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+        ServerSocket serverSocket = serverSocketChannel.socket();
+        serverSocket.bind(address);
+
+        //创建buffer
+        ByteBuffer byteBuffer = ByteBuffer.allocate(4096);
+        while(true){
+            SocketChannel socketChannel = serverSocketChannel.accept();
+            int readcount = 0;
+            while(-1 != readcount){
+                try {
+                    readcount = socketChannel.read(byteBuffer);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+                //倒带这个buffer position = 0 mark 作废
+                byteBuffer.rewind();
+            }
+        }
+    }
+}
