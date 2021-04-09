c3ff6a76d761dd7e2a57077187c4a8bb4377463f
lijinlin <jinlin.li@haitun88.com>
2021-04-09 08:28:28 +0800

    netty实现群聊

---
+++ src/main/java/com/lijinlin/project/learn/netty/websocket/MyServer.java
@@ -0,0 +1,58 @@
+package com.lijinlin.project.learn.netty.websocket;
+
+import com.lijinlin.project.learn.netty.heartbeat.MyServerHandler;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.stream.ChunkedWriteHandler;
+import io.netty.handler.timeout.IdleStateHandler;
+
+import java.util.concurrent.TimeUnit;
+
+public class MyServer {
+
+    public static void main(String[] args) throws Exception {
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+        try{
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup,workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    //在bossGroup增加一个日志处理器
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ChannelPipeline pipeline = ch.pipeline();
+                            //因为基于http协议，因此使用http的编码和解码器
+                            pipeline.addLast(new HttpServerCodec());
+                            //是以块方式写，添加一个ChunkedWriter处理器
+                            pipeline.addLast(new ChunkedWriteHandler());
+
+                            /*
+                            说明
+                            1.因为http的数据在传输过程中是分段的，HttpObjectAggregator就是可以将多个段聚合起来
+                            2.这就是为什么，当浏览器发送大量数据时，就会发出多次http请求的原因
+                             */
+                            pipeline.addLast(new HttpObjectAggregator(8192));
+                        }
+                    });
+            //启动服务器
+            ChannelFuture channelFuture = serverBootstrap.bind(7000).sync();
+            channelFuture.channel().closeFuture().sync();
+        }finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+
+
+    }
+}
