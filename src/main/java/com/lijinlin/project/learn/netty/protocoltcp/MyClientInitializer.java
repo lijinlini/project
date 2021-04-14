682c50f404424c1d0a72e19e09bd400da1a818e2
lijinlin <jinlin.li@haitun88.com>
2021-04-14 08:29:35 +0800

    netty

---
+++ src/main/java/com/lijinlin/project/learn/netty/protocoltcp/MyClientInitializer.java
@@ -0,0 +1,17 @@
+package com.lijinlin.project.learn.netty.protocoltcp;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+
+
+public class MyClientInitializer extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+        //加入编码器
+        pipeline.addLast(new MymessageEncoder());
+        pipeline.addLast(new MyClientHandler());
+
+    }
+}
---
+++ src/main/java/com/lijinlin/project/learn/netty/protocoltcp/MyServerHandler.java
@@ -0,0 +1,31 @@
+package com.lijinlin.project.learn.netty.protocoltcp;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+import java.nio.charset.Charset;
+import java.util.UUID;
+
+public class MyServerHandler extends SimpleChannelInboundHandler<MessageProtocol> {
+    private int count;
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, MessageProtocol msg) throws Exception {
+       //接收到数据，并处理
+        int len = msg.getLen();
+        byte[] content = msg.getContent();
+        System.out.println("服务器接收到信息如下");
+        System.out.println("长度="+len);
+        System.out.println("内容="+new String(content,Charset.forName("utf-8")));
+        System.out.println("服务器接收到协议包数量=" + (this.count++));
+        // todo 回复小心
+    }
+}
