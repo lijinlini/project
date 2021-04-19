2adb2699e782ba0d06190cbef02f96365f09d610
lijinlin <jinlin.li@haitun88.com>
2021-04-19 08:11:51 +0800

    netty实现dubbo完成

--- src/main/java/com/lijinlin/project/learn/netty/dubborpc/netty/NettyClient.java
+++ src/main/java/com/lijinlin/project/learn/netty/dubborpc/netty/NettyClient.java
@@ -20,10 +20,13 @@ public class NettyClient {
     //创建线程池
     private static ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
     private static NettyClientHandler client;
+    private int count = 0;
 
     //编写方法使用代理模式，获取一个代理对象
     public Object getBean(final Class<?> serviceClass,final String providerName){
         return Proxy.newProxyInstance(Thread.currentThread().getContextClassLoader(),new Class<?>[]{serviceClass},(proxy,method,args)->{
+            System.out.println("(proxy,method,args) 进入...." + (++count) + "次");
+
            //{}部分的代码，客户端每调用一次hello，就会进入到该代码
             if(client == null){
                initClient();
--- src/main/java/com/lijinlin/project/learn/netty/dubborpc/netty/NettyClientHandler.java
+++ src/main/java/com/lijinlin/project/learn/netty/dubborpc/netty/NettyClientHandler.java
@@ -16,33 +16,40 @@ public class NettyClientHandler extends ChannelInboundHandlerAdapter implements 
     //(1)与服务器的连接创建成功后，就会被调用
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println(" channelActive 被调用");
         //因为我们在其他方法会使用到ctx
         context = ctx;
+    }
+
+    //(4)收到服务器的数据后，调用方法
+    @Override
+    public synchronized void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println(" channelRead 被调用");
+        result = msg.toString();
+        //唤醒等待的线程
+        notify();
     }
 
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
         ctx.close();
     }
-    //(4)收到服务器的数据后，调用方法
-    @Override
-    public synchronized void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        result = msg.toString();
-        //唤醒等待的线程
-        notify();
-    }
+
 
     //(3)被代理对象调用，发送数据给服务器，->wait -> 等待被唤醒(channelRead) ->返回结果(3)->5
     @Override
     public synchronized Object call() throws Exception {
+        System.out.println(" call1 被调用");
         context.writeAndFlush(para);
         //进行wait,等待channelRead方法获取到服务器的结果后唤醒。
         wait();
+        System.out.println(" call2 被调用");
         //服务方返回的结果
         return result;
     }
     //(2)
     void setPara(String para){
+        System.out.println(" setPara 被调用");
         this.para = para;
     }
 }
