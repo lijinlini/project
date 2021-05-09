275e93953e94201d5c372f5f9323e960607c1325
lijinlin <jinlin.li@haitun88.com>
2021-05-09 23:52:43 +0800

    二叉树层级遍历，给二叉树添加next指针

--- src/main/java/com/lijinlin/project/learn/datastructureandalgorithm/binarysorttree/AddNext.java
+++ src/main/java/com/lijinlin/project/learn/datastructureandalgorithm/binarysorttree/AddNext.java
@@ -8,24 +8,28 @@ package com.lijinlin.project.learn.datastructureandalgorithm.binarysorttree;
 public class AddNext {
     /**
      * 给二叉树增加next节点，指向同一层的下一个节点
+     *
      * @author lijinlin
      * @date 2021/5/8 17:37
      */
     public static void main(String[] args) {
-        TreeNode root = new TreeNode(1);
-
+        TreeNode root1 = new TreeNode(1);
+        TreeNode root2 = new TreeNode(2);
+        TreeNode root3 = new TreeNode(3);
+        TreeNode root4 = new TreeNode(4);
+        TreeNode root5 = new TreeNode(5);
+        TreeNode root6 = new TreeNode(6);
+        TreeNode root7 = new TreeNode(7);
+        root1.left = root2;
+        root1.right = root3;
+        root2.left = root4;
+        root2.right = root5;
+        root3.left = root6;
+        root3.right = root7;
+        connectNext(root1);
+        System.out.println(root1.value + "," + root2.value + "," + root2.next.value + "," + root4.value + "," + root4.next.value + "," + root4.next.next.value + "," + root4.next.next.next.value);
     }
-
-    /**
-     * 给定一个root节点把root节点下的数据都添加同层一下一个指向，root的next为null
-     *
-     * @param root
-     * @author lijinlin
-     * @date 2021/5/8 17:51
-     */
-    public void connectNext(TreeNode root) {
-        //当前节点
-        TreeNode curNode = root;
+    public static void connectNext(TreeNode curNode) {
         //当前节点下一行第一个节点
         TreeNode firstChild = null;
         //当前系欸但下一行最后一个节点
@@ -38,7 +42,7 @@ public class AddNext {
             }
             //设置子节点的next关系，3种情况 第一种左右都有，第二种左有，第三种右有
             if (curNode.left != null && curNode.right != null) {
-                /* todo 不懂*/
+                /* 当前节点的左右节点不为空，并且当前节点的下一行有最后一个节点，最后一个节点指向当前节点的左节点，左节点指向当前节点右节点，最后一个节点指向当前节点右节点*/
                 if (lastChild != null) {
                     lastChild.next = curNode.left;
                 }
@@ -46,26 +50,27 @@ public class AddNext {
                 curNode.left.next = curNode.right;
                 lastChild = curNode.right;
             } else if (curNode.left != null) {
-                /* todo 不懂*/
+                /*当前节点右节点空，所以最后一个节点是当前节点左节点，最后一个节点指向当前节点左节点*/
                 if (lastChild != null) {
                     lastChild.next = curNode.left;
                 }
                 /*end*/
                 lastChild = curNode.left;
             } else if (curNode.right != null) {
-                /* todo 不懂*/
+                /*当前节点左节点为null，最后一个节点都是当前节点右节点，最后一个节点下一个指向当前节点右节点*/
                 if (lastChild != null) {
                     lastChild.next = curNode.right;
                 }
                 /*end*/
                 lastChild = curNode.right;
             }
+
             //设置下一个节点，如果本层已经遍历完毕，移到下一层的第一个子节点
             if (curNode.next != null) {
-                /* todo 不懂*/
+                /* 当不换行的时候，当前节点平行右移*/
                 curNode = curNode.next;
-            }else{
-                /* todo 不懂*/
+            } else {
+                /*当换行的时候，当前节点变成下一行第一个节点，清空下一行第一个和最后一个节点记录*/
                 curNode = firstChild;
                 firstChild = null;
                 lastChild = null;
@@ -78,10 +83,11 @@ public class AddNext {
 
     //有下一个节点的二叉树
     static class TreeNode {
-        private int value;
-        private TreeNode left;
-        private TreeNode right;
-        private TreeNode next;
+        public int value;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode next;
+
         public TreeNode(int value) {
             this.value = value;
         }
---
+++ src/main/java/com/lijinlin/project/learn/datastructureandalgorithm/binarysorttree/SortByLayer.java
@@ -0,0 +1,125 @@
+package com.lijinlin.project.learn.datastructureandalgorithm.binarysorttree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class SortByLayer {
+
+    public static void main(String[] args) {
+        TreeNode root3 = new TreeNode(3);
+        TreeNode root9 = new TreeNode(9);
+        TreeNode root20 = new TreeNode(20);
+        TreeNode root15 = new TreeNode(15);
+        TreeNode root7 = new TreeNode(7);
+        root3.left = root9;
+        root3.right = root20;
+        root9.left = root15;
+        root9.right = root7;
+        ArrayList<ArrayList<Integer>> list = levelOrder(root3);
+        System.out.println("[");
+        for(ArrayList<Integer> list1 : list){
+            System.out.print("[");
+            for(Integer value : list1){
+                System.out.print(value+",");
+            }
+            System.out.println("],");
+        }
+        System.out.println("]");
+
+    }
+
+    /**
+     * 给定一个root节点把root节点下的数据都添加同层一下一个指向，root的next为null
+     * 求二叉树的层序遍历
+     * 限定语言：Kotlin、Typescript、Python、C++、Groovy、Rust、Java、Go、Scala、Javascript、Ruby、Swift、Php、Python 3
+     * 给定一个二叉树，返回该二叉树层序遍历的结果，（从左到右，一层一层地遍历）
+     * 例如：
+     * 给定的二叉树是{3,9,20,#,#,15,7},
+     * <p>
+     * 该二叉树层序遍历的结果是
+     * [
+     * [3],
+     * [9,20],
+     * [15,7]
+     * ]
+     * <p>
+     * Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+     * For example:
+     * Given binary tree{3,9,20,#,#,15,7},
+     * <p>
+     * return its level order traversal as:
+     * [
+     * [3],
+     * [9,20],
+     * [15,7]
+     * ]
+     *
+     * @author lijinlin
+     * @date 2021/5/8 17:51
+     */
+    public static ArrayList<ArrayList<Integer>> levelOrder(TreeNode root) {
+        //使用队列实现
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        TreeNode curNode = root;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(curNode);
+
+        while (!queue.isEmpty()) {
+            //获取队列大小
+            int queueSize = queue.size();
+            ArrayList<Integer> lineList = new ArrayList<>();
+            for (int i = 0; i < queueSize; i++) {
+                //取出队列最早的数据
+                curNode = queue.poll();
+                if (curNode.left != null) {
+                    queue.add(curNode.left);
+                }
+                if (curNode.right != null) {
+                    queue.add(curNode.right);
+                }
+                lineList.add(curNode.value);
+            }
+            res.add(lineList);
+        }
+        return res;
+    }
+
+
+    //有下一个节点的二叉树
+    static class TreeNode {
+        public int value;
+        public TreeNode left;
+        public TreeNode right;
+
+
+        public TreeNode(int value) {
+            this.value = value;
+        }
+    }
+
+     /* public List<List<Integer>> levelOrder(TreeNode root) {
+
+            List<List<Integer>> res = new ArrayList<List<Integer>>();
+            Queue<TreeNode> queue = new LinkedList<>();
+            if(root == null) return res;
+            queue.add(root);
+            while(!queue.isEmpty()){
+                int cur = queue.size();
+                ArrayList<Integer> tmp = new ArrayList<Integer>();
+                for(int i=0;i<cur;i++){
+                    TreeNode t= queue.poll();
+                    tmp.add(t.val);
+                    if(t.left != null)
+                        queue.add(t.left);
+                    if(t.right != null)
+                        queue.add(t.right);
+                }
+                res.add(tmp);
+            }
+            return res;
+        }*/
+}
