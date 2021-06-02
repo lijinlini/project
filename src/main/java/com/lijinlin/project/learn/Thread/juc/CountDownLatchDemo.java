36339cd021374aea8e45577a371d1a4f38ed4c3f
jl.li21 <Ljl139691425512>
2021-06-02 17:32:48 +0800

    迷宫问题

---
+++ src/main/java/com/lijinlin/project/learn/Thread/juc/CountDownLatchDemo.java
@@ -0,0 +1,49 @@
+package com.lijinlin.project.learn.Thread.juc;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * @author lijinlin
+ * @date2021年06月02日 17:09
+ */
+public class CountDownLatchDemo {
+    public static void main(String[] args) throws InterruptedException {
+        CountDownLatch countDownLatchOne = new CountDownLatch(1);
+        CountDownLatch countDownLatchFive = new CountDownLatch(5);
+
+        for (int i = 0; i < 5; i++){
+            new Thread(new MyRunnable(countDownLatchOne,countDownLatchFive)).start();
+        }
+        System.out.println("用于触发处于等待状态的线程开始工作......");
+        System.out.println("用于触发处于等待状态的线程工作完成，等待状态线程开始工作......");
+        countDownLatchOne.countDown();
+        countDownLatchFive.await();
+        System.out.println("五个子线程都执行完了");
+    }
+
+    public static class MyRunnable implements Runnable{
+        private final CountDownLatch countDownLatchOne;
+        private final CountDownLatch countDownLatchFive;
+
+        public MyRunnable(CountDownLatch countDownLatchOne, CountDownLatch countDownLatchFive) {
+            this.countDownLatchOne = countDownLatchOne;
+            this.countDownLatchFive = countDownLatchFive;
+        }
+        @Override
+        public void run() {
+            try{
+                System.out.println("等待主线程执行完毕，获得开始执行信号...");
+                countDownLatchOne.await();
+                System.out.println("处于等待的线程开始自己预期工作......");
+                countDownLatchFive.countDown();
+                System.out.println("完成预期工作，发出完成信号...");
+
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+
+}
