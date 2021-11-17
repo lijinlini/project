ed58f7308b2bec48b0708adb814781b92447c36f
lijinlin <l5976877>
2021-11-17 19:47:26 +0800

    静态代理

---
+++ src/main/java/com/lijinlin/project/learn/DesignPattern/proxy/staticProxy/ITeacherDao.java
@@ -0,0 +1,6 @@
+package com.lijinlin.project.learn.DesignPattern.proxy.staticProxy;
+
+public interface ITeacherDao {
+    //授课方法
+    void teach();
+}
---
+++ src/main/java/com/lijinlin/project/learn/DesignPattern/proxy/staticProxy/TeacherDaoProxy.java
@@ -0,0 +1,17 @@
+package com.lijinlin.project.learn.DesignPattern.proxy.staticProxy;
+//代理对象，静态代理
+public class TeacherDaoProxy implements ITeacherDao{
+    //目标对象，通过接口来聚合
+    private ITeacherDao target;
+
+    public TeacherDaoProxy(ITeacherDao target) {
+        this.target = target;
+    }
+
+    @Override
+    public void teach() {
+        System.out.println("代理开始。。。");
+        target.teach();
+        System.out.println("代理结束。。。");
+    }
+}
