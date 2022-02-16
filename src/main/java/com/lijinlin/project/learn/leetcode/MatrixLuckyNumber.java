c3da5974b57d4095bbfc99e205db3158fe3bb395
lijinlin <jinlin.li@haitun88.com>
2022-02-17 00:02:00 +0800

    矩阵幸运数

--- src/main/java/com/lijinlin/project/learn/leetcode/MatrixLuckyNumber.java
+++ src/main/java/com/lijinlin/project/learn/leetcode/MatrixLuckyNumber.java
@@ -5,29 +5,44 @@ import java.util.List;
 
 public class MatrixLuckyNumber {
     public static void main(String[] args) {
+        int[][] matrix1 = new int[3][3];
+        int[][] matrix2 = new int[3][4];
+        matrix2[0][0] = 1;
+        matrix2[0][1] = 10;
+        matrix2[0][2] = 4;
+        matrix2[0][3] = 2;
+        matrix2[1][0] = 9;
+        matrix2[1][1] = 3;
+        matrix2[1][2] = 8;
+        matrix2[1][3] = 7;
+        matrix2[2][0] = 15;
+        matrix2[2][1] = 16;
+        matrix2[2][2] = 17;
+        matrix2[2][3] = 12;
+        //List<Integer> b = luckyNumbers(matrix2);
 
+
+        int[][] matrix3 = new int[2][2];
+        matrix3[0][0] = 7;
+        matrix3[0][1] = 8;
+        matrix3[1][0] = 1;
+        matrix3[1][1] = 2;
+        List<Integer> c = luckyNumbers(matrix3);
+        System.out.println(c);
 
     }
 
-    public List<Integer> luckyNumbers (int[][] matrix) {
+    public static List<Integer> luckyNumbers (int[][] matrix) {
         int numLine = matrix.length;
         int numColumn = matrix[0].length;
-        int numPre = 0;
-
         int[] numMinLineArray = new int[numLine];
         for(int i = 0; i < numLine; i++ ){
             //同一行中最小
-            int numLineMin = 0;
+            int numLineMin = matrix[i][0];
             for(int j = 0; j < numColumn; j++ ){
-                if(j > 0){
-                    //上一个数
-                    numPre = matrix[i][j-1];
-                }else{
-                    numPre = matrix[i][j];
-                }
                 //当前数
                 int numCur = matrix[i][j];
-                if(numCur <= numPre){
+                if(numCur <= numLineMin){
                     numLineMin = numCur;
                 }
             }
@@ -36,17 +51,11 @@ public class MatrixLuckyNumber {
         int[] numMaxColumnArray = new int[numColumn];
         for(int i = 0; i < numColumn; i++ ){
             //同一列中最大
-            int numColumnMax = 0;
+            int numColumnMax = matrix[0][i];
             for(int j = 0; j < numLine; j++ ){
-                if(j > 0){
-                    //上一个数
-                    numPre = matrix[i][j-1];
-                }else{
-                    numPre = matrix[i][j];
-                }
                 //当前数
-                int numCur = matrix[i][j];
-                if(numCur >= numPre){
+                int numCur = matrix[j][i];
+                if(numCur >= numColumnMax){
                     numColumnMax = numCur;
                 }
             }
@@ -55,12 +64,11 @@ public class MatrixLuckyNumber {
         List<Integer> result = new ArrayList<>(1);
         for(int i = 0 ; i < numMinLineArray.length;i++){
             for(int j = 0; j < numMaxColumnArray.length; j++ ){
-                if(i == j){
-                    result.add(i);
+                if(numMinLineArray[i] == numMaxColumnArray[j]){
+                    result.add(numMinLineArray[i]);
                 }
             }
         }
-
         return result;
     }
 }
