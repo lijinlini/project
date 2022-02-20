051e11aa7028387f992158f5311abeef22a30d7f
lijinlin <jinlin.li@haitun88.com>
2022-02-20 23:39:57 +0800

    骑士在棋盘上的概率

--- src/main/java/com/lijinlin/project/learn/leetcode/KinghtProbability.java
+++ src/main/java/com/lijinlin/project/learn/leetcode/KinghtProbability.java
@@ -4,10 +4,36 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class KinghtProbability {
+    static int[][] dirs = {{-2, -1}, {-2, 1}, {2, -1}, {2, 1}, {-1, -2}, {-1, 2}, {1, -2}, {1, 2}};
+
     public static void main(String[] args) {
-        double reuslt = knightProbability(8,30,6,4);
+        double reuslt = knightProbability1(3, 2, 0, 0);
         System.out.println(reuslt);
     }
+
+    public static double knightProbability1(int n, int k, int row, int column) {
+        //步数 行 列
+        double[][][] piece = new double[k + 1][n][n];
+        for (int step = 0; step <= k; step++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (step == 0) {
+                        piece[step][i][j] = 1;
+                    } else {
+                        for (int[] dir : dirs) {
+                            int ni = i + dir[0], nj = j + dir[1];
+                            if (ni >= 0 && ni < n && nj >= 0 && nj < n) {
+                                //说明当前点的周围8个点之一在范围内
+                                piece[step][i][j] += piece[step - 1][ni][nj] / 8;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return piece[k][row][column];
+    }
+
     /**
      * 688. 骑士在棋盘上的概率
      *
@@ -54,7 +80,7 @@ public class KinghtProbability {
             in++;
         }
         double curResult = in / 8.0;
-        if(k > 1){
+        if (k > 1) {
             double nextResult = 1;
             //每一步都是8分之一的概率，所以每一次都乘以0.125
             double curResult1 = knightProbability(n, k - 1, row - 2, column - 1);
@@ -65,27 +91,28 @@ public class KinghtProbability {
             double curResult6 = knightProbability(n, k - 1, row - 1, column + 2);
             double curResult7 = knightProbability(n, k - 1, row + 1, column + 2);
             double curResult8 = knightProbability(n, k - 1, row + 2, column + 1);
-            if(curResult1 != 0){
+            if (curResult1 != 0) {
                 nextResult = curResult1;
             }
-            if(curResult2 != 0){
+            if (curResult2 != 0) {
                 nextResult = curResult2;
             }
-            if(curResult3 != 0){
+            if (curResult3 != 0) {
                 nextResult = curResult3;
             }
-            if(curResult4 != 0){
+            if (curResult4 != 0) {
                 nextResult = curResult4;
             }
-            if(curResult5 != 0){
+            if (curResult5 != 0) {
                 nextResult = curResult5;
             }
-            if(curResult6 != 0){
+            if (curResult6 != 0) {
                 nextResult = curResult6;
-            }if(curResult7 != 0){
+            }
+            if (curResult7 != 0) {
                 nextResult = curResult7;
             }
-            if(curResult8 != 0){
+            if (curResult8 != 0) {
                 nextResult = curResult8;
             }
 
