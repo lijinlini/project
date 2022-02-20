029b0a145fa0f16819425c86e6cd36941c7482c5
lijinlin <jinlin.li@haitun88.com>
2022-02-20 17:55:23 +0800

    骑士在棋盘上的概率

--- src/main/java/com/lijinlin/project/learn/leetcode/KinghtProbability.java
+++ src/main/java/com/lijinlin/project/learn/leetcode/KinghtProbability.java
@@ -5,8 +5,7 @@ import java.util.List;
 
 public class KinghtProbability {
     public static void main(String[] args) {
-        double reuslt = knightProbability(3,1,0,0);
-        //double s = 2/8;
+        double reuslt = knightProbability(8,30,6,4);
         System.out.println(reuslt);
     }
     /**
@@ -24,47 +23,73 @@ public class KinghtProbability {
             return 1;
         }
         //不在棋盘中并且一步不走则一定不在棋盘中
-        if (k <= 0 && (row > n - 1 || row < 0 || column > n - 1 || column < 0)) {
+        if (k <= 0 || row > n - 1 || row < 0 || column > n - 1 || column < 0) {
             return 0;
         }
         //每一步只有8个格子可以走 递归的思路
-        double in = 8;
-        if (row - 2 < 0 || column - 1 < 0 || row - 2 > n - 1 || column - 1 > n - 1) {
-            in--;
+        double in = 0;
+        if (row - 2 >= 0 && column - 1 >= 0 && row - 2 <= n - 1 && column - 1 <= n - 1) {
+            //在范围内0
+            in++;
         }
-        if (row - 1 < 0 || column - 2 < 0 || row - 1 > n - 1 || column - 2 > n - 1) {
-            in--;
+        if (row - 1 >= 0 && column - 2 >= 0 && row - 1 <= n - 1 && column - 2 <= n - 1) {
+            in++;
         }
-        if (row + 1 < 0 || column - 2 < 0 || row + 1 > n - 1 || column - 2 > n - 1) {
-            in--;
+        if (row + 1 >= 0 && column - 2 >= 0 && row + 1 <= n - 1 && column - 2 <= n - 1) {
+            in++;
         }
-        if (row + 2 < 0 || column - 1 < 0 || row + 2 > n - 1 || column - 1 > n - 1) {
-            in--;
+        if (row + 2 >= 0 && column - 1 >= 0 && row + 2 <= n - 1 && column - 1 <= n - 1) {
+            in++;
         }
-        if (row - 2 < 0 || column + 1 < 0 || row - 2 > n - 1 || column + 1 > n - 1) {
-            in--;
+        if (row - 2 >= 0 && column + 1 >= 0 && row - 2 <= n - 1 && column + 1 <= n - 1) {
+            in++;
         }
-        if (row - 1 < 0 || column + 2 < 0 || row - 1 > n - 1 || column + 2 > n - 1) {
-            in--;
+        if (row - 1 >= 0 && column + 2 >= 0 && row - 1 <= n - 1 && column + 2 <= n - 1) {
+            in++;
         }
-        if (row + 1 < 0 || column + 2 < 0 || row + 1 > n - 1 || column + 2 > n - 1) {
-            in--;
+        if (row + 1 >= 0 && column + 2 >= 0 && row + 1 <= n - 1 && column + 2 <= n - 1) {
+            in++;
         }
-        if (row + 2 < 0 || column + 1 < 0 || row + 2 > n - 1 || column + 1 > n - 1) {
-            in--;
+        if (row + 2 >= 0 && column + 1 >= 0 && row + 2 <= n - 1 && column + 1 <= n - 1) {
+            in++;
         }
         double curResult = in / 8.0;
-        if(k > 0){
+        if(k > 1){
+            double nextResult = 1;
             //每一步都是8分之一的概率，所以每一次都乘以0.125
-            double curResult1 = 0.125 * knightProbability(n, k - 1, row - 2, column - 1);
-            double curResult2 = 0.125 * knightProbability(n, k - 1, row - 1, column - 2);
-            double curResult3 = 0.125 * knightProbability(n, k - 1, row + 1, column - 2);
-            double curResult4 = 0.125 * knightProbability(n, k - 1, row + 2, column - 1);
-            double curResult5 = 0.125 * knightProbability(n, k - 1, row - 2, column + 1);
-            double curResult6 = 0.125 * knightProbability(n, k - 1, row - 1, column + 2);
-            double curResult7 = 0.125 * knightProbability(n, k - 1, row + 1, column + 2);
-            double curResult8 = 0.125 * knightProbability(n, k - 1, row + 2, column + 1);
-            curResult = curResult1 + curResult2 + curResult3 + curResult4 + curResult5 + curResult6 + curResult7 + curResult8;
+            double curResult1 = knightProbability(n, k - 1, row - 2, column - 1);
+            double curResult2 = knightProbability(n, k - 1, row - 1, column - 2);
+            double curResult3 = knightProbability(n, k - 1, row + 1, column - 2);
+            double curResult4 = knightProbability(n, k - 1, row + 2, column - 1);
+            double curResult5 = knightProbability(n, k - 1, row - 2, column + 1);
+            double curResult6 = knightProbability(n, k - 1, row - 1, column + 2);
+            double curResult7 = knightProbability(n, k - 1, row + 1, column + 2);
+            double curResult8 = knightProbability(n, k - 1, row + 2, column + 1);
+            if(curResult1 != 0){
+                nextResult = curResult1;
+            }
+            if(curResult2 != 0){
+                nextResult = curResult2;
+            }
+            if(curResult3 != 0){
+                nextResult = curResult3;
+            }
+            if(curResult4 != 0){
+                nextResult = curResult4;
+            }
+            if(curResult5 != 0){
+                nextResult = curResult5;
+            }
+            if(curResult6 != 0){
+                nextResult = curResult6;
+            }if(curResult7 != 0){
+                nextResult = curResult7;
+            }
+            if(curResult8 != 0){
+                nextResult = curResult8;
+            }
+
+            curResult = nextResult * curResult;
         }
         return curResult;
     }
