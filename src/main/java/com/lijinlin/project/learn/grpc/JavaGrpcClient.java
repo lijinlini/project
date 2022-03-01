95eb590bc0f8e98e0d1449727fc039189a121149
lijinlin <lijinlin@apusapps.com>
2022-03-01 18:19:40 +0800

    有效括号

--- src/main/java/com/lijinlin/project/learn/grpc/JavaGrpcClient.java
+++ src/main/java/com/lijinlin/project/learn/grpc/JavaGrpcClient.java
@@ -5,12 +5,15 @@ import io.grpc.ManagedChannelBuilder;
 import java_test.TestServiceGrpc;
 import org.springframework.stereotype.Component;
 
+import java.util.concurrent.TimeUnit;
+
 @Component
 public class JavaGrpcClient {
     private Channel channel = channel();
 
     public <Result> Result run(Functional<TestServiceGrpc.TestServiceBlockingStub,Result> functional){
         TestServiceGrpc.TestServiceBlockingStub testServiceBlockingStub = TestServiceGrpc.newBlockingStub(channel);
+        testServiceBlockingStub.withDeadlineAfter(1,TimeUnit.SECONDS);
         return functional.run(testServiceBlockingStub);
     }
 
