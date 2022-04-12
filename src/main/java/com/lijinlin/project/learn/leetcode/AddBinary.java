def7395b5d63618896c0b7beaaa2501d814bdce4
lijinlin <lijinlin@apusapps.com>
2022-04-12 17:07:54 +0800

    二进制字符串相加

---
+++ src/main/java/com/lijinlin/project/learn/leetcode/AddBinary.java
@@ -0,0 +1,70 @@
+package com.lijinlin.project.learn.leetcode;
+
+/**
+ * 67. 二进制求和
+ * 给你两个二进制字符串，返回它们的和（用二进制表示）。
+ * <p>
+ * 输入为 非空 字符串且只包含数字 1 和 0。
+ * 示例 1:
+ * <p>
+ * 输入: a = "11", b = "1"
+ * 输出: "100"
+ * <p>
+ * <p>
+ * <p>
+ * 示例 2:
+ * <p>
+ * 输入: a = "1010", b = "1011"
+ * 输出: "10101"
+ */
+public class AddBinary {
+    public static void main(String[] args) {
+        String a = "11";
+        String b = "1";
+        System.out.println(addBinary(a, b));
+    }
+
+    public static String addBinary(String a, String b) {
+        int aLength = a.length();
+        int bLength = b.length();
+        int maxLength = aLength > bLength ? aLength : bLength;
+        int addNum = 0;
+        StringBuilder result = new StringBuilder("");
+        while (aLength != 0 || bLength != 0 || addNum != 0) {
+            int a1 = 0;
+            if (aLength == maxLength) {
+                a1 = Integer.valueOf(String.valueOf(a.charAt(aLength - 1)));
+            } else {
+                if(aLength > 0){
+                    a1 = Integer.valueOf(String.valueOf(a.charAt(aLength - 1)));
+                }
+            }
+            if(aLength > 0){
+                aLength = --aLength;
+            }
+            int b1 = 0;
+            if (bLength == maxLength) {
+                b1 = Integer.valueOf(String.valueOf(b.charAt(bLength - 1)));
+            }else{
+                if(bLength > 0){
+                    b1 = Integer.valueOf(String.valueOf(b.charAt(bLength - 1)));
+                }
+            }
+            if(bLength > 0){
+                bLength = --bLength;
+            }
+            int curNum = a1 + b1 + addNum;
+            addNum = 0;
+            if (curNum - 2 == 1) {
+                curNum = 1;
+                addNum = 1;
+            }
+            if (curNum - 2 == 0) {
+                curNum = 0;
+                addNum = 1;
+            }
+            result.append(curNum);
+        }
+        return result.reverse().toString();
+    }
+}
