bf8ffb1325c8fbe7b772add8cfa32f25a3bc1a79
lijinlin <lijinlin@apusapps.com>
2022-06-10 16:41:03 +0800

    滑动时间窗口查找最大子串

--- src/main/java/com/lijinlin/project/learn/leetcode/normal/lengthOfLongestSubstring.java
+++ src/main/java/com/lijinlin/project/learn/leetcode/normal/lengthOfLongestSubstring.java
@@ -1,5 +1,12 @@
 package com.lijinlin.project.learn.leetcode.normal;
 
+import com.alibaba.druid.sql.visitor.functions.Char;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.TreeMap;
+
 /**
  * 3. 无重复字符的最长子串
  * 给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
@@ -22,9 +29,34 @@ package com.lijinlin.project.learn.leetcode.normal;
 public class lengthOfLongestSubstring {
 
     public static void main(String[] args) {
-
+        String  s = "pwwkew";
+        System.out.println(lengthOfLongestSubstring(s));
     }
-    public int lengthOfLongestSubstring(String s) {
-        return 0;
+    public static int lengthOfLongestSubstring(String s) {
+        if(s == null || s.equals("")){
+            return 0;
+        }
+        //滑动窗口
+        int startIndex = 0;
+        int endIndex = 0;
+        HashMap<Character,Integer> slidingWindow = new HashMap<>();
+        int maxNum = 0;
+        char[] chars = s.toCharArray();
+        int i = 0;
+        for (char c : chars){
+            if(slidingWindow.containsKey(c)){
+                int preRepeatIndex = slidingWindow.get(c);
+                if(preRepeatIndex >= startIndex){
+                    startIndex = preRepeatIndex + 1;
+                }
+            }
+            endIndex ++;
+            slidingWindow.put(c,i);
+            i++;
+            if(endIndex - startIndex > maxNum){
+                maxNum = endIndex - startIndex;
+            }
+        }
+        return maxNum;
     }
 }
