a4071f07465e1e893f2de64f277c4492fcb675d7
lijinlin <lijinlin@apusapps.com>
2022-06-15 16:18:44 +0800

    最大子回文串东态规划方法

--- src/main/java/com/lijinlin/project/learn/leetcode/normal/LongestPalindrome.java
+++ src/main/java/com/lijinlin/project/learn/leetcode/normal/LongestPalindrome.java
@@ -14,7 +14,49 @@ package com.lijinlin.project.learn.leetcode.normal;
 public class LongestPalindrome {
     public static void main(String[] args) {
         String s = "cbbd";
-        System.out.println(longestPalindromeViolence(s));
+        System.out.println(longestPalindromeDynamic(s));
+    }
+
+    /**
+     * 东态规划方法计算最大回文子串
+     *
+     * @param s
+     * @return
+     */
+    private static String longestPalindromeDynamic(String s) {
+        char[] charArray = s.toCharArray();
+        int len = charArray.length;
+        //回文串的最大长度
+        int maxLength = 1;
+        //回文串最大长度对应的起始位置
+        int begin = 0;
+        Boolean[][] rememberTable = new Boolean[len][len];
+        //单个字母都是回文
+        for(int i = 0;i<len;i++){
+            rememberTable[i][i] = true;
+        }
+        //i 是右侧坐标 j是左侧坐标 循环按照先列再行的顺序便利
+        for (int i = 1; i < len; i++) {
+            for (int j = 0; j < i; j++) {
+                if(charArray[i] != charArray[j]){
+                    //不相同一定不是回文串
+                    rememberTable[i][j] = false;
+                }else{
+                    //相同情况下要判断 从i到j的数量，如果数量小于3则相当于两边相等中间只有一个，一定是回文串，如果i-1 = j+1则也是回文串
+                    if(i - j < 3 || rememberTable[i - 1][j + 1]){
+                        rememberTable[i][j] = true;
+                    }else{
+                        rememberTable[i][j] = false;
+                    }
+                }
+
+                if(rememberTable[i][j] && i - j + 1 > maxLength){
+                    maxLength = i - j + 1;
+                    begin = j;
+                }
+            }
+        }
+        return s.substring(begin, begin + maxLength);
     }
 
     /**
@@ -34,13 +76,13 @@ public class LongestPalindrome {
         int begin = 0;
         for (int i = 0; i < len - 1; i++) {
             for (int j = i + 1; j < len; j++) {
-                if (j - i + 1 > maxLength && validPalindromic(charArray,i,j)){
+                if (j - i + 1 > maxLength && validPalindromic(charArray, i, j)) {
                     begin = i;
                     maxLength = j - i + 1;
                 }
             }
         }
-        return s.substring(begin,begin + maxLength);
+        return s.substring(begin, begin + maxLength);
     }
 
     /**
@@ -56,4 +98,6 @@ public class LongestPalindrome {
         }
         return true;
     }
+
+
 }
