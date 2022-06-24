012dc11c926173b2ff73e99d1fabda4877f7bdf6
lijinlin <lijinlin@apusapps.com>
2022-06-24 18:32:33 +0800

    数字转罗马文

---
+++ src/main/java/com/lijinlin/project/learn/leetcode/normal/IntToRoman.java
@@ -0,0 +1,31 @@
+package com.lijinlin.project.learn.leetcode.normal;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class IntToRoman {
+    public static void main(String[] args) {
+
+    }
+    public String intToRoman(int num) {
+        int[] numArray = {1000,500,100,50,10,5,1};
+        String[] romanSignalArray = {"M","D","C","L","X","V","I"};
+       /* Map<Integer,String> map = new HashMap<>(16);
+        map.put(1000,"M");
+        map.put(500,"D");
+        map.put(100,"C");
+        map.put(50,"L");
+        map.put(10,"X");
+        map.put(5,"V");
+        map.put(1,"I");*/
+        boolean isEnd = false;
+        while (isEnd){
+
+
+
+        }
+
+        return "";
+    }
+}
