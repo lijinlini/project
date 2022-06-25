8d40ba3153df9a5b94215e063b88ff214b05c55d
lijinlin <lijinlin@apusapps.com>
2022-06-25 18:15:21 +0800

    三数之和

---
+++ src/main/java/com/lijinlin/project/learn/leetcode/normal/ThreeSum.java
@@ -0,0 +1,27 @@
+package com.lijinlin.project.learn.leetcode.normal;
+
+import java.util.List;
+
+/**
+ * 15. 三数之和
+ * 给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有和为 0 且不重复的三元组。
+ *
+ * 注意：答案中不可以包含重复的三元组。
+ *
+ * 输入：nums = [-1,0,1,2,-1,-4]
+ * 输出：[[-1,-1,2],[-1,0,1]]
+ *
+ * 输入：nums = []
+ * 输出：[]
+ *
+ * 输入：nums = [0]
+ * 输出：[]
+ */
+public class ThreeSum {
+    public static void main(String[] args) {
+
+    }
+    public List<List<Integer>> threeSum(int[] nums) {
+        return null;
+    }
+}
