ae6d84d09b2183875774d31dd999739b69b4543d
lijinlin <lijinlin@apusapps.com>
2022-06-25 18:11:17 +0800

    数字转罗马文

--- src/main/java/com/lijinlin/project/learn/leetcode/normal/IntToRoman.java
+++ src/main/java/com/lijinlin/project/learn/leetcode/normal/IntToRoman.java
@@ -6,26 +6,30 @@ import java.util.Set;
 
 public class IntToRoman {
     public static void main(String[] args) {
-
+        System.out.println(intToRoman(901));
     }
-    public String intToRoman(int num) {
-        int[] numArray = {1000,500,100,50,10,5,1};
-        String[] romanSignalArray = {"M","D","C","L","X","V","I"};
-       /* Map<Integer,String> map = new HashMap<>(16);
-        map.put(1000,"M");
-        map.put(500,"D");
-        map.put(100,"C");
-        map.put(50,"L");
-        map.put(10,"X");
-        map.put(5,"V");
-        map.put(1,"I");*/
+    public static String intToRoman(int num) {
+        int[] numArray = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String[] romanSignalArray = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
         boolean isEnd = false;
-        while (isEnd){
-
-
-
+        int tempNum = num;
+        //数组位置用来
+        int i = 0;
+        StringBuilder resultSignal = new StringBuilder("");
+        while (!isEnd){
+            String curSignal = "";
+            if(tempNum >= numArray[i]){
+                tempNum = tempNum - numArray[i];
+                curSignal = romanSignalArray[i];
+            }
+            if(tempNum < numArray[i]){
+                i++;
+            }
+            resultSignal.append(curSignal);
+            if(tempNum == 0){
+                isEnd = true;
+            }
         }
-
-        return "";
+        return resultSignal.toString();
     }
 }
